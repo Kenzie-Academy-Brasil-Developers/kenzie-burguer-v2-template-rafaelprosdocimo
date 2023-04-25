54e08594a46c5bb387e75b20e4ce5d94cfc123ed
rafael-eva <124081906+rafael-eva@users.noreply.github.com>
2023-04-25 14:23:12 -0300

    feat login e cadastro

--- src/components/Form/Input/index.tsx
+++ src/components/Form/Input/index.tsx
@@ -1,14 +1,20 @@
+import { ForwardedRef, forwardRef, InputHTMLAttributes } from 'react';
 import { StyledInputContainer } from '../../../styles/form';
 import { StyledParagraph } from '../../../styles/typography';
-
-const Input = ({ id }) => (
+import { FieldError } from "react-hook-form";
+interface IInputPros extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  error?: FieldError;
+}
+const Input = forwardRef(({label, error, id,...rest}: IInputPros, ref: ForwardedRef<HTMLInputElement>) => {
+  return(
   <div>
-    <StyledInputContainer>
-      <input type='text' id={id} placeholder=' ' />
-      <label htmlFor={id}>Teste</label>
-    </StyledInputContainer>
-    <StyledParagraph fontColor='red'>Erro</StyledParagraph>
-  </div>
-);
-
+      <StyledInputContainer>
+        <input ref={ref} {...rest} type='text' id={id} placeholder=' ' />
+        {label? <label htmlFor={id}>{label}</label>: null}
+      </StyledInputContainer>
+      {error? <StyledParagraph fontColor='red'>{error.message}</StyledParagraph>: null}
+    </div>
+  );
+});
 export default Input;
